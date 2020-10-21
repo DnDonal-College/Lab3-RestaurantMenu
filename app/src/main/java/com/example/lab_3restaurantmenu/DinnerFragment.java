88c5218132e09bf5e1732d22d69faec8e0a42bb6
Donal <17233577@studentmail.ul.ie>
2020-10-21 19:14:05 +0100

    Initial commit

---
+++ app/src/main/java/com/example/lab_3restaurantmenu/DinnerFragment.java
@@ -0,0 +1,92 @@
+package com.example.lab_3restaurantmenu;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link DinnerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class DinnerFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+    public String dinnerSelection;
+    RadioGroup radioGroup;
+    RadioButton radioButton;
+
+    public DinnerFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment DinnerFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static DinnerFragment newInstance(String param1, String param2) {
+        DinnerFragment fragment = new DinnerFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_dinner, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        radioGroup = getView().findViewById(R.id.radioGroupD);
+        Button next = view.findViewById(R.id.dinnerNext);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int radioId = radioGroup.getCheckedRadioButtonId();
+
+                radioButton = getView().findViewById(radioId);
+                dinnerSelection = (String) radioButton.getText();
+                Bundle bundle = getArguments();
+                bundle.putString("Option 2",dinnerSelection);
+                Navigation.findNavController(v).navigate(R.id.action_dinnerFragment_to_drinkFragment,bundle);
+            }
+        });
+    }
+}
